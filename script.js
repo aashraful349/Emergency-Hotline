f1ee2388301b2b73fd548dccc130a0a1f7a75a03
aashraful349 <aashraful349@gmail.com>
2025-10-06 02:59:50 +0600

    somehow history section is working but i dont know how..gonna hava look at it

--- script.js
+++ script.js
@@ -21,6 +21,17 @@ for(const callButton of callButtonPressed){
                 time:new Date().toLocaleTimeString()
             }
             serviceInfo.push(info);
+            const historySection=document.getElementById('history');
+            historySection.appendChild(document.createElement('div')).classList.add('call-history-list','p-4','flex','justify-between','items-center');
+            const callHistoryList=historySection.lastChild;
+            callHistoryList.appendChild(document.createElement('div'));
+            callHistoryList.lastChild.appendChild(document.createElement('h1')).classList.add('font-semibold','text-[18px]');
+            callHistoryList.lastChild.firstChild.innerText=info.service;
+            callHistoryList.lastChild.appendChild(document.createElement('p')).classList.add('text-gray-400','text-[18px]');
+            callHistoryList.lastChild.lastChild.innerText='999';
+            callHistoryList.appendChild(document.createElement('div'));
+            callHistoryList.lastChild.appendChild(document.createElement('p')).classList.add('text-[18px]','text-gray-400');
+            callHistoryList.lastChild.firstChild.innerText=info.time;
         }
         else{
             alert('You have no coins left. Please recharge your coins.');
@@ -28,4 +39,3 @@ for(const callButton of callButtonPressed){
         
     })
 }
-console.log(serviceInfo);
