8c16cf1ddef7f0fed13b6baabcb75b0407f1520f
aashraful349 <aashraful349@gmail.com>
2025-10-06 02:51:58 +0600

    worked on the history ...still needs work

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let serviceInfo=[];
+
 const heartIconPressed=document.getElementsByClassName('heart-icon');
 for(const heartIcon of heartIconPressed){
     heartIcon.addEventListener('click',function(e){
@@ -5,3 +7,25 @@ for(const heartIcon of heartIconPressed){
         newHeartCount=heartCount+1;
         document.getElementById('heart-count').innerText=newHeartCount;
     })}
+
+const callButtonPressed=document.getElementsByClassName('call-button');
+for(const callButton of callButtonPressed){
+    const service=callButton.parentNode.parentNode.firstElementChild.innerText;
+    callButton.addEventListener('click',function(e){
+        const coins=parseInt(document.getElementById('coin-count').innerText);
+        if(coins>=20){
+            newCoinCount=coins-20;
+            document.getElementById('coin-count').innerText=newCoinCount;
+            const info={
+                service:service,
+                time:new Date().toLocaleTimeString()
+            }
+            serviceInfo.push(info);
+        }
+        else{
+            alert('You have no coins left. Please recharge your coins.');
+        }
+        
+    })
+}
+console.log(serviceInfo);
